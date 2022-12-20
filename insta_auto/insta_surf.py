91282353330d9d60ef5123a592bce62dc9bb2a1a
Ansh Bajpai <ansh.suditi@gmail.com>
2022-12-20 13:48:53 +0530

    hi

--- insta_auto/insta_surf.py
+++ insta_auto/insta_surf.py
@@ -2,9 +2,9 @@ from bot import main
 class insta(main):
     pass
     
-x = insta('y',6,"_ansh_bajpai_","ansh@999")    
+x = insta('y',6,"_ansh_bajpai_","ansh@999",'ansh')  
 while True:
     x.open()
-    x.surf(50,3)  
+    x.surf(50,2)  
     x.close()
     break
